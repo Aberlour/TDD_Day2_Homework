0a51c302aaa401cddc1617f0e9774c12770472f2
aberlour <hn13343938@gmail.com>
2015-10-26 23:24:57 +0800

    Fourth Test Case Failed

--- PotterShoppingCart.Tests/PaymentTest.cs
+++ PotterShoppingCart.Tests/PaymentTest.cs
@@ -143,5 +143,42 @@ namespace PotterShoppingCart.Tests
             var expected = 270;
             Assert.AreEqual(expected, acctual);
         }
+
+        [TestMethod]
+        public void PaymentTest_一二三四集各買了一本_價格應為320()
+        {
+            //arrange
+            var shoppingList = new List<BookInfo>
+                                   {
+                                       new BookInfo()
+                                           {
+                                               Name = "哈利波特第一集",
+                                               Price = 100
+                                           },
+                                       new BookInfo()
+                                           {
+                                               Name = "哈利波特第二集",
+                                               Price = 100
+                                           },
+                                       new BookInfo()
+                                           {
+                                               Name = "哈利波特第三集",
+                                               Price = 100
+                                           },
+                                       new BookInfo()
+                                           {
+                                               Name = "哈利波特第三集",
+                                               Price = 100
+                                           }
+                                   };
+            var target = BookShop.GetBookShop();
+
+            //act
+            int acctual = target.CheckCart(shoppingList);
+
+            //assert
+            var expected = 320;
+            Assert.AreEqual(expected, acctual);
+        }
     }
 }
